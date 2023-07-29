5901cff56c6a93a367239ec7264b006ed96df83e
Marco Antonio Galian <marcogalian@hotmail.com>
2023-07-29 09:53:46 +0200

    corregida vista movil

--- resources/js/Pages/Codes/Index.jsx
+++ resources/js/Pages/Codes/Index.jsx
@@ -82,7 +82,7 @@ const Index = ({auth, codes}) => {
             <div className='max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 mt-2'>
                 {
                     codes.map(code =>
-                        <div key={code.id} className='bg-white shadow-sm rounded-md mb-4'>
+                        <div key={code.id} className='bg-white shadow-sm rounded-md mb-4 overflow-auto'>
                             <Code  
                                 code={code}
                                 changeStateModal={changeStateModal} 
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = ({ auth, codes, changeStateModal, postId, deletePost, idPost }) 
         <>
             <Head title="Welcome" />
             <div className="relative bg-slate-100 h-full w-full">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right bg-gray-200 w-full">
+                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right bg-white shadow-md w-full">
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
@@ -39,7 +39,7 @@ const Welcome = ({ auth, codes, changeStateModal, postId, deletePost, idPost }) 
                 <div className='max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 mt-20'>
                     {
                         codes.map(code =>
-                            <div key={code.id} className='bg-white shadow-sm rounded-md mb-4'>
+                            <div key={code.id} className='bg-white shadow-sm rounded-md mb-4 overflow-auto'>
                                 <Code  
                                     code={code}
                                     changeStateModal={changeStateModal} 
